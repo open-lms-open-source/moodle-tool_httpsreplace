d9512f2784f919d10d11bbfd6730e4d7ae2185a5
Mark Nielsen <mark@moodlerooms.com>
2017-09-13 15:15:02 -0700

    INT-12069: Update release/requires for 3.3

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2017063000; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016120500; // Requires this Moodle version.
-$plugin->release   = '3.2.3';
+$plugin->requires  = 2017051500; // Requires this Moodle version.
+$plugin->release   = '3.3.1';
 $plugin->component = 'tool_httpsreplace'; // Full name of the plugin (used for diagnostics).
 
 $plugin->maturity  = MATURITY_STABLE; // This version's maturity level.
